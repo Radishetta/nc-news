8b7f741fef33256310c0e6ebc929da5f360ddfa2
radishetta <geneshaft@gmail.com>
2024-05-29 14:27:31 +0100

    Adds /api/articles/article_id/comments endpoint with error handling and associated tests

--- models/articles.model.js
+++ models/articles.model.js
@@ -17,14 +17,24 @@ exports.fetchArticles = () => {
     .query(
       `SELECT articles.author, articles.title, articles.article_id,
       articles.topic, articles.created_at, articles.votes, 
-      articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count 
-      FROM comments
-      JOIN articles ON comments.article_id = articles.article_id
+      articles.article_img_url, COUNT(comments.article_id)::INT AS comment_count 
+      FROM articles
+      LEFT JOIN comments ON comments.article_id = articles.article_id
       GROUP BY articles.article_id
       ORDER BY articles.created_at DESC;`
     )
     .then(({ rows: articles }) => {
       if (!articles.length) return Promise.reject({ status: 404, msg: "Articles not found" });
       else return articles;
+    });
+};
+
+exports.checkArticleExists = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows: articles }) => {
+      if (!articles.length) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
     });
 };
